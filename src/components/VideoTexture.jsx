54e022b140febbb54ba9f4d93205bcf63e2223c9
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2024-07-05 15:30:17 +0530

    Update VideoTexture.jsx

--- src/components/VideoTexture.jsx
+++ src/components/VideoTexture.jsx
@@ -1,26 +1,12 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import { useVideoTexture } from '@react-three/drei';
 
+
 const VideoTexture = ({ url }) => {
-  const videoRef = useRef();
-  const texture = useVideoTexture(videoRef);
 
-  useEffect(() => {
-    const video = videoRef.current;
-    if (video) {
-      video.crossOrigin = 'Anonymous'; // Ensure cross-origin permissions if needed
-      video.loop = true; // Loop the video
-      video.muted = false; // Ensure audio is not muted
-      video.autoplay = true; // Autoplay the video
-    }
-  }, []);
+  const texture = useVideoTexture(url);
 
-  return (
-    <>
-      <video ref={videoRef} src={url} />
-      <meshBasicMaterial map={texture} toneMapped={false} />
-    </>
-  );
+  return <meshBasicMaterial map={texture} toneMapped={false} />;
 };
 
-export default VideoTexture;
+export default VideoTexture; 
